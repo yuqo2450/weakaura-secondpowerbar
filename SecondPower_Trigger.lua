9740a89343ccbad68da08a7447534c4f4a08c9f4
daniel rossa <daniel.rossa@rossa-fam.local>
2021-04-25 11:40:56 +0200

    renamed variables

--- SecondPower_Trigger.lua
+++ SecondPower_Trigger.lua
@@ -1,45 +1,45 @@
 function(allstates,event,arg1,arg2,...)
 
-    local unitPowerIndex,unitPowerName,maxPower = aura_env.GetUnitPowerType("player");
-    local unitClass = UnitClassBase("player");
+    local powerIndex,powerName,maxPower = aura_env.GetUnitPowerType("player");
+    local class = UnitClassBase("player");
    
     if event == "UNIT_POWER_UPDATE" then
         
-        if arg1 ~= "player" or arg2 ~= unitPowerName then
+        if arg1 ~= "player" or arg2 ~= powerName then
             return true;
         end
         
-        if arg1 == "player" and arg2 == unitPowerName then          
-            aura_env.SetPowerValue(allstates,maxPower,unitPowerIndex);
+        if arg1 == "player" and arg2 == powerName then          
+            aura_env.SetPowerValue(allstates,maxPower,powerIndex);
             return true;
         end
 
-    elseif event == "RUNE_POWER_UPDATE" then
+    elseif event == "RUNE_POWER_UPDATE" and class == "DEATHKNIGHT" then
 
         aura_env.SetDKRunes(allstates,maxPower);
         return true;
         
     elseif event == "PLAYER_ENTERING_WORLD" then
 
-        if unitClass == "DEATHKNIGHT" then
+        if class == "DEATHKNIGHT" then
             aura_env.SetDKRunes(allstates,maxPower);
         else
-            aura_env.CreateStates(allstates,maxPower,unitPowerIndex);
-         end
+            aura_env.CreateStates(allstates,maxPower,powerIndex);
+        end
         return true;
 
     elseif event == "PLAYER_TALENT_UPDATE" then
 
         aura_env.ClearStates(allstates);
-        aura_env.CreateStates(allstates,maxPower,unitPowerIndex);
+        aura_env.CreateStates(allstates,maxPower,powerIndex);
         return true;
 
-    elseif event =="UPDATE_SHAPESHIFT_FORM" and unitClass == "DRUID" then
+    elseif event =="UPDATE_SHAPESHIFT_FORM" and class == "DRUID" then
 
         local _,catActive = GetShapeshiftFormInfo(2);
         
         if catActive then
-            aura_env.CreateStates(allstates,maxPower,unitPowerIndex);
+            aura_env.CreateStates(allstates,maxPower,powerIndex);
         elseif next(allstates) == nil then
             return true;
         else
