1d8cabc1f00686f1df26173be389562dffb95c0c
daniel rossa <daniel.rossa@rossa-fam.local>
2021-04-25 10:30:17 +0200

    dk runes inverse now

--- SecondPower_Text.lua
+++ SecondPower_Text.lua
@@ -5,4 +5,7 @@ function()
     if not aura_env.config.defaultColor then
         aura_env.region:Color(aura_env.SetBarColor(class));
     end
+    if class == "DEATHKNIGHT" then
+        aura_env.region:SetInverse(true);
+    end
 end
