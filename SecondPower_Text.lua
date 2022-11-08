9be43dd22b6133a0e0e9fa4dd43134505a356f12
Daniel Rossa <danielrossa92@gmail.com>
2022-11-08 19:45:27 +0100

    added support for DK runes

--- SecondPower_Text.lua
+++ SecondPower_Text.lua
@@ -1,11 +1,12 @@
 function()
     
-    local class = UnitClassBase("player");
-    
-    if not aura_env.config.defaultColor then
-        aura_env.region:Color(aura_env.SetBarColor(class));
-    end
-    if class == "DEATHKNIGHT" then
-        aura_env.region:SetInverse(true);
-    end
+  local class = UnitClassBase("player");
+  
+  if not aura_env.config.defaultColor then
+      aura_env.region:Color(aura_env.SetBarColor(class));
+  end
+  if class == "DEATHKNIGHT" then
+      aura_env.region:SetInverse(true);
+  end
 end
+
