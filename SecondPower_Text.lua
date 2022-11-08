169eede40f08c903e165cff8ca35bfe3732c2b41
Daniel Rossa <danielrossa92@gmail.com>
2022-11-08 19:53:50 +0100

    linted code

--- SecondPower_Text.lua
+++ SecondPower_Text.lua
@@ -3,10 +3,10 @@ function()
   local class = UnitClassBase("player");
   
   if not aura_env.config.defaultColor then
-      aura_env.region:Color(aura_env.SetBarColor(class));
+    aura_env.region:Color(aura_env.SetBarColor(class));
   end
   if class == "DEATHKNIGHT" then
-      aura_env.region:SetInverse(true);
+    aura_env.region:SetInverse(true);
   end
 end
 
