6c5d715b58b563982fe0bbbdedbd6244406040e5
yuqo2450 <79540477+yuqo2450@users.noreply.github.com>
2023-09-10 17:46:06 +0200

    Added stagger as a resource (#3)
    * added: working stagger config
    
    * added: vscode lua config for repositroy
    
    * removed: trailing spaces
    
    * added: stagger is now displayed in different colours for different stages
    
    * changed: generated new import string for aura
    
    * fixed: bugg where entering combat caused a lua error
    
    * changed: refactored code for better readability
    
    * added: function to test if states are set correctly
    
    * changed: shortened filenames cause prefix is not required anymore
    
    * added: local variable to reduce funcion calls
    
    * changed: refactored code for readability
    
    * updated: import string for weak aura
    
    * added: default changelog file
    
    * changed: file structure
    
    * added: changelog file

---
+++ text.lua
@@ -0,0 +1,14 @@
+function()
+    
+  local class = UnitClassBase("player");
+  if not aura_env.config.defaultColor then
+    aura_env.region:Color(aura_env.SetBarColor(class));
+  end
+  if class == "DEATHKNIGHT" then
+    aura_env.region:SetInverse(true);
+  elseif class == "MONK" and GetSpecialization() == 1 then
+    aura_env.region:SetOrientation("HORIZONTAL");
+    aura_env.region:SetRegionWidth(150);
+    aura_env.region:SetRegionHeight(15);
+  end
+end
