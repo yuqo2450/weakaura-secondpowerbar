a14a1818ff3a72e5aff62c8d252e4bedd72c0d00
Daniel Rossa <danielrossa92@gmail.com>
2023-10-29 16:03:50 +0100

    changed: moved code for stagger to seperate trigger

--- trigger_01.lua
+++ trigger_01.lua
@@ -44,29 +44,7 @@ function(allstates,event,arg1,arg2,...)
       aura_env.ClearStates(allstates);
     end
     return true;
-  elseif event == "UNIT_AURA" then
-    if arg1 ~= "player" then
-      return false;
-    end
-
-    --[[
-      When states are not tested, there will be an LUA error when loading the aura in combat
-      Testing the states fixes this bug
-    ]]
-    aura_env.TestStates(allstates, maxPower, powerIndex)
-    if arg2['addedAuras'] ~= nil and string.find(arg2['addedAuras'][1]['name'], 'Stagger') then
-      aura_env.staggerAuraInstanceID = arg2['addedAuras'][1]['auraInstanceID'];
-      aura_env.SetPowerValue(allstates,maxPower,powerIndex);
-      return true;
-    elseif arg2['updatedAuraInstanceIDs'] and arg2['updatedAuraInstanceIDs'][1] == aura_env.staggerAuraInstanceID then
-      aura_env.SetPowerValue(allstates,maxPower,powerIndex);
-      return true;
-    elseif arg2['removedAuraInstanceIDs'] and arg2['removedAuraInstanceIDs'][1] == aura_env.staggerAuraInstanceID then
-      aura_env.SetPowerValue(allstates,maxPower,powerIndex);
-      aura_env.staggerAuraInstanceID = nil;
-      return true;
-    else
-      return false
-    end
+  else
+    return false;
   end
 end
