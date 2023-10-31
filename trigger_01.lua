8f928f070538954543c4a9f4a06d1744e116d8a2
Daniel Rossa <danielrossa92@gmail.com>
2023-10-31 19:13:44 +0100

    changed: removed unit power seperate trigger cause not working as described in docs

--- trigger_01.lua
+++ trigger_01.lua
@@ -1,12 +1,12 @@
 --[[
   Power creation trigger
-  Events: PLAYER_SPECIALIZATION_CHANGED,TRAIT_CONFIG_UPDATED,UPDATE_SHAPESHIFT_FORM,PLAYER_ENTERING_WORLD
+  Events: PLAYER_SPECIALIZATION_CHANGED,TRAIT_CONFIG_UPDATED,UPDATE_SHAPESHIFT_FORM,PLAYER_ENTERING_WORLD,UNIT_POWER_UPDATE
   ]]
 function(allstates,event,arg1,arg2,...)
   local class = UnitClassBase("player");
   local powerIndex,powerName,maxPower = aura_env.GetUnitPowerType("player");
 
- if event == "PLAYER_ENTERING_WORLD" then
+  if event == "PLAYER_ENTERING_WORLD" then
     if class == "DEATHKNIGHT" then
       aura_env.SetDKRunes(allstates,maxPower);
     else
@@ -27,6 +27,24 @@ function(allstates,event,arg1,arg2,...)
     else
       aura_env.ClearStates(allstates);
     end
+    return true;
+
+  elseif event == "UNIT_POWER_UPDATE" then
+    if arg1 ~= "player" then
+      return false;
+    end
+
+    if class == "DEATHKNIGHT" then
+      return false;
+    end
+
+    --[[
+      There exists an issue with event PLAYER_ENTERING_WORLD and function UnitPowerMax().
+      On the named event UnitPowerMax() retunrs for Chi and HolyPower a value smaller than the actual.
+      This causes a lua error that is fixed with the following code.
+    ]]
+    aura_env.TestStates(allstates, maxPower, powerIndex);
+    aura_env.SetPowerValue(allstates,maxPower,powerIndex);
     return true;
 
   else
