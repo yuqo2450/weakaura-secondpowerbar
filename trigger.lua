e79548c500c982db13eb4b4787780090ffc6ff83
Daniel Rossa <79540477+yuqo2450@users.noreply.github.com>
2023-11-02 10:17:30 +0100

    Fixed an issue concerning stagger and purifying brew (#9)
    * changed: renamed files to add more triggers to aura
    
    * changed: moved code for stagger to seperate trigger
    
    * changed: split different functionality into seperate triggers
    
    * changed: removed test prints and listening to TRAIT_CONFIG_UPDATED
    
    * changed: refactored code for better readability
    
    * changed: removed unit power seperate trigger cause not working as described in docs
    
    * changed: removed dk rune seperate trigger cause not working as described in docs
    
    * changed: fixed lua errors
    
    * changed: fixed state creation for all events
    
    * changed: stagger seperate trigger cause not working as described in docs
    
    * changed: added function to set stagger value
    
    * changed: setting stagger value for every aura update to fix issue #8
    
    * changed: updated import string for wago.io
    
    * changed: updated changelog

--- trigger.lua
+++ trigger.lua
@@ -1,41 +1,26 @@
+--[[
+  Power creation trigger
+  Events: PLAYER_SPECIALIZATION_CHANGED,TRAIT_CONFIG_UPDATED,PLAYER_ENTERING_WORLD,UPDATE_SHAPESHIFT_FORM,UNIT_POWER_UPDATE,RUNE_POWER_UPDATE,UNIT_AURA
+  ]]
 function(allstates,event,arg1,arg2,...)
-
   local class = UnitClassBase("player");
   local powerIndex,powerName,maxPower = aura_env.GetUnitPowerType("player");
 
-  if event == "UNIT_POWER_UPDATE" then
-    if arg1 ~= "player" then
-      return false
-    end
-    --[[
-      There exists an issue with event PLAYER_ENTERING_WORLD and function UnitPowerMax().
-      On the named event UnitPowerMax() retunrs for Chi and HolyPower a value smaller than the actual.
-      This causes a lua error that is fixed with the following code.
-    ]]
-    aura_env.TestStates(allstates, maxPower, powerIndex)
-
-    if arg2 == powerName and class ~= "DEATHKNIGHT" then
-      aura_env.SetPowerValue(allstates,maxPower,powerIndex);
-    end
-    return true;
-  elseif event == "RUNE_POWER_UPDATE" and class == "DEATHKNIGHT" then
-    aura_env.SetDKRunes(allstates,maxPower);
-    return true;
-  elseif event == "PLAYER_ENTERING_WORLD" then
+  if event == "PLAYER_ENTERING_WORLD" then
     if class == "DEATHKNIGHT" then
       aura_env.SetDKRunes(allstates,maxPower);
     else
       aura_env.CreateStates(allstates,maxPower,powerIndex);
     end
     return true;
-  elseif next({"TRAIT_CONFIG_UPDATED", "PLAYER_SPECIALIZATION_CHANGED"}) == event then
+
+  elseif "PLAYER_SPECIALIZATION_CHANGED" == event or "TRAIT_CONFIG_UPDATED" == event then
     aura_env.ClearStates(allstates);
-    -- aura_env.CreateStates(allstates,maxPower,powerIndex);
+    aura_env.CreateStates(allstates,maxPower,powerIndex);
     return true;
+
   elseif event =="UPDATE_SHAPESHIFT_FORM" and class == "DRUID" then
-
     local _,catActive = GetShapeshiftFormInfo(2);
-
     if catActive then
       aura_env.CreateStates(allstates,maxPower,powerIndex);
     elseif next(allstates) == nil then
@@ -44,29 +29,49 @@ function(allstates,event,arg1,arg2,...)
       aura_env.ClearStates(allstates);
     end
     return true;
+
+  elseif event == "UNIT_POWER_UPDATE" then
+    if arg1 ~= "player" then
+      return false;
+    end
+
+    if arg2 ~= powerName then
+      return false;
+    end
+
+    if class == "DEATHKNIGHT" then
+      return false;
+    elseif class == "MONK" and GetSpecialization() == 1 then
+      return false;
+    end
+
+    aura_env.TestStates(allstates, maxPower, powerIndex);
+    aura_env.SetPowerValue(allstates,maxPower,powerIndex);
+    return true;
+
+  elseif event == "RUNE_POWER_UPDATE" then
+    if class ~= "DEATHKNIGHT" then
+      return false;
+    end
+
+    aura_env.TestStates(allstates, maxPower, powerIndex);
+    aura_env.SetDKRunes(allstates, maxPower);
+    return true;
+
   elseif event == "UNIT_AURA" then
     if arg1 ~= "player" then
       return false;
     end
 
-    --[[
-      When states are not tested, there will be an LUA error when loading the aura in combat
-      Testing the states fixes this bug
-    ]]
-    aura_env.TestStates(allstates, maxPower, powerIndex)
-    if arg2['addedAuras'] ~= nil and string.find(arg2['addedAuras'][1]['name'], 'Stagger') then
-      aura_env.staggerAuraInstanceID = arg2['addedAuras'][1]['auraInstanceID'];
-      aura_env.SetPowerValue(allstates,maxPower,powerIndex);
-      return true;
-    elseif arg2['updatedAuraInstanceIDs'] and arg2['updatedAuraInstanceIDs'][1] == aura_env.staggerAuraInstanceID then
-      aura_env.SetPowerValue(allstates,maxPower,powerIndex);
-      return true;
-    elseif arg2['removedAuraInstanceIDs'] and arg2['removedAuraInstanceIDs'][1] == aura_env.staggerAuraInstanceID then
-      aura_env.SetPowerValue(allstates,maxPower,powerIndex);
-      aura_env.staggerAuraInstanceID = nil;
-      return true;
-    else
-      return false
+    if class ~= "MONK" and GetSpecialization() ~= 1 then
+      return false;
     end
+
+    aura_env.TestStates(allstates, maxPower, powerIndex);
+    aura_env.SetPowerValue(allstates,maxPower,powerIndex);
+    return true;
+
+  else
+    return false;
   end
 end
